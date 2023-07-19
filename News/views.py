ed8a727ace27609b8e74518a9776adbea2ba33f9
Grigory-Dripak <126253741+Grigory-Dripak@users.noreply.github.com>
2023-07-19 23:43:42 +0300

    D13.4 Logging added

--- News/views.py
+++ News/views.py
@@ -8,6 +8,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from .models import Post, Category, Subscription
 from .forms import PostForm
 from .filters import PostFilter
+from django.core.cache import cache
 # from django.http import HttpResponseRedirect
 
 
@@ -40,9 +41,21 @@ class PostsSearch(ListView):
 class PostDetail(LoginRequiredMixin, DetailView):
     raise_exception = True
     model = Post
+    #queryset = Post.objects.all()
     template_name = 'post_details.html'
     context_object_name = 'post_details'
-    pk_url_kwarg = 'id'
+    #pk_url_kwarg = 'id'
+
+"""    def get_object(self, *args, **kwargs):  # переопределяем метод получения объекта, как ни странно
+        obj = cache.get(f'post-{self.kwargs["pk"]}', None)
+        # кэш очень похож на словарь, и метод get действует так же.
+        # Он забирает значение по ключу, если его нет, то забирает None.
+        # если объекта нет в кэше, то получаем его и записываем в кэш
+        if not obj:
+            obj = super().get_object(queryset=self.queryset)
+            cache.set(f'post-{self.kwargs["pk"]}', obj)
+            return obj
+"""
 
 
 class NewsCreate(PermissionRequiredMixin, CreateView):
